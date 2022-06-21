349e05afa0b1959dc23c1a041631966cf6de4e50
martin023 <mainamartin023@gmail.com>
2022-06-21 14:26:20 +0300

    Adding basic models and html templates

--- hunter/models.py
+++ hunter/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location_name = models.CharField(max_length=40)
+
+
+    def __str__(self):
+
+        return self.location_name
+
+
+class House(models.Model):
+    title = models.CharField(max_length=40)
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    type = models.Choices()
+    units = models.IntegerField()
+    kitchen = models.IntegerField()
+    parking = models.IntegerChoices()
+
+
+    def __str__(self):
+
+        return self.title
