90f9dc6a249008d4221af5b12af62b8bac54498e
martin023 <mainamartin023@gmail.com>
2022-06-24 12:38:46 +0300

    Update

--- hunter/urls.py
+++ hunter/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.home ,name='homepage'),
     path('gallery/',views.all_houses ,name='gallery'),
-    path('house/<pk>',views.get_house,name="gethouse"),
+    path('house/<pk>/',views.get_house,name="gethouse"),
     path('add_house/',views.add_house,name='addhouse'),
     path('house/<pk>/edit/',views.update_house,name='update_house'),
     path('house/<pk>/delete',views.delete_house,name='delete')
